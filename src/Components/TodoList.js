674affe1268229d90f45078db7b5ced243bb33a3
alidarwiche <darwiche2005@utexas.edu>
2018-07-28 14:29:27 -0500

    starting todo list

---
+++ src/Components/TodoList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './TodoList.css';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="section">
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
