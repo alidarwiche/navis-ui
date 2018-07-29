7c08b6a3b97865573d81e86d1c9c27a3c7c84199
alidarwiche <darwiche2005@utexas.edu>
2018-07-28 20:33:20 -0500

    have basic components of todo list section

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import Check from 'react-icons/lib/fa/check'
+import Times from 'react-icons/lib/ti/times'
+import './TodoItem.css'
+
+
+class TodoItem extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="TodoItem">
+          <div style={{backgroundColor: 'white', width:'100%', display: 'inline-flex'}}>
+            <Check size={20} style={{height:'100%',color:'#e9e9e9', padding:'0px 10px 0px 10px'}} />
+            <p style={{margin:'10px'}}> help </p>
+          </div>
+          <Times size={20} style={{height:'100%', padding:'10px'}} />
+      </div>
+    )
+  }
+}
+
+export default TodoItem;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react'
-import './TodoList.css';
+import Plus from 'react-icons/lib/fa/plus'
+import TodoItem from './TodoItem.js'
+import './TodoList.css'
 
 class TodoList extends Component {
+  // private method
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log(e.target.value);
+    }
+  }
+
+
+
   render() {
     return (
-      <div className="section">
-        <h1>Hello World!</h1>
+      <div className="Todo">
+        <div className="Todo-column" style={{textAlign: 'left'}} >
+          <h1>To-Do List</h1>
+          <div style={{display:'flex', border: '2px solid #ccc', borderRadius: '4px', boxSizing: 'border-box'}}>
+            <input placeholder='What needs to be done?' style={{width:'100%'}} type='text' name='todo_item' onKeyPress={this._handleKeyPress} />
+            <Plus style={{padding: '10px', backgroundColor: 'white', borderRight: '4px'}}/>
+          </div>
+          <p>To add an item to your To-Do list type it in the field above and press enter or click the plus icon.</p>
+        </div>
+        <div className="Todo-column">
+          <TodoItem />
+        </div>
       </div>
     )
   }
